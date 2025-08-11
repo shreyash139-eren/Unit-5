ba50910f72accc994f83aae66f29ffdadb4fa043
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-08-11 15:05:14 +0530

    evaluation

--- VideoGameCatalogAPIEvaluation/controllers/game.controller.js
+++ VideoGameCatalogAPIEvaluation/controllers/game.controller.js
@@ -38,7 +38,7 @@ const updateGame = async (req, res) => {
       if (!game) {
         return res.status(404).json({ message: "No game found" });
       }
-      await GamerModel.findByIdAndUpdate(id, req.body);
+      await GameModel.findByIdAndUpdate(id, req.body);
       res.status(200).json({ message: "Gmae updated" });
     } catch (error) {
       res.status(500).json({ message: "Something went wrong" });
